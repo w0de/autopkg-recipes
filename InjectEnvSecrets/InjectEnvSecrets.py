a59b5971eaf551e9b66bce9b4222f3e5a8dc285e
Harry S <harry.seeber@gusto.com>
2021-08-31 18:44:37 -0600

    add stub processor

---
+++ InjectEnvSecrets/InjectEnvSecrets.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+from autopkglib import PlistEditor
+
+class InjectEnvSecrets(PlistEditor):
+    description = __doc__
+    input_variables = {
+        "secrets": {
+            "required": True,
+            "description": (
+                "List of names of secrets to find in env and inject."
+            ),
+        },
+        "hard_fail": {
+            "required": False,
+            "default": True,
+            "description": (
+                "Hard fail if secret cannot be found."
+            )
+        }
+    }
+
+    def main(self):
+        print self.env
+        sys.exit(1)
